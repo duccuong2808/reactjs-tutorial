8382b53f42190139d1abb374750ce8b9a7588ad7
Duc Cuong <cuong.nd@altplus.com.vn>
2017-03-09 11:13:59 +0700

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const config = {
+  entry: './main.js',
+
+  output: {
+    // path: './',
+    filename: 'index.js',
+  },
+
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
