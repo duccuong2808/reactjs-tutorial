f9a8454c3a962f3314ae47f5052c10f7aaa9e03e
Duc Cuong <cuong.nd@altplus.com.vn>
2017-03-10 09:40:49 +0700

    03. Props - default props

--- main.js
+++ main.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(
-  <App headerProp = "Header from props..." contentProp = "Content from props..."/>,
-  document.getElementById('app')
-);
-
-export default App;
+ReactDOM.render(<App/>, document.getElementById('app'));
