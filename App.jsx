3a36ae3fac3a1b011e47786496933988c0f4ecc0
Duc Cuong <cuong.nd@altplus.com.vn>
2017-03-15 15:06:01 +0700

    12. Animations - Step B: Enter and leave animation

--- App.jsx
+++ App.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
-var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+let ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: ['hello', 'world', 'click', 'me']};
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ]);
+    this.setState({items: newItems});
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  }
 
   render() {
+    const items = this.state.items.map((item, i) => (
+      <div key={item} onClick={() => this.handleRemove(i)}>
+        {item}
+      </div>
+    ));
+
     return (
       <div>
-        <ReactCSSTransitionGroup transitionName = "example"
-                                 transitionAppear = {true} transitionAppearTimeout = {50}
-                                 transitionEnter = {false} transitionLeave = {false}>
-
-          <h1>My Element...</h1>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={1}
+          transitionLeaveTimeout={10000}>
+          {items}
         </ReactCSSTransitionGroup>
       </div>
     );
